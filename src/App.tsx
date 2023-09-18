0dcb61e8c091c27510e041bcd02013c1c131153e
Austin <appiaha66@gmail.com>
2023-09-18 08:51:18 +0000

    pagination works

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
 import FilterChips from "./FilterChips";
+import { Paginator } from 'primereact/paginator';
 
 function App() {
   const filters = [
@@ -20,7 +21,6 @@ function App() {
   ];
 
   const [activeFilter, setActiveFilter] = useState("All");
-
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
@@ -28,9 +28,14 @@ function App() {
   };
 
   // api call
+
   type Universities = {
-    name : string;
+    name: string;
   }
+
+  const [first, setFirst] = useState(0);
+  const [rows, ] = useState(10);
+  const [totalRecords, setTotalRecords] = useState(0); // Total number of universities
   const [ghanaianUniversities, setGhanaianUniversities] = useState<Universities[]>([]);
 
   useEffect(() => {
@@ -44,6 +49,7 @@ function App() {
         }
         const data = await response.json();
         setGhanaianUniversities(data);
+        setTotalRecords(data.length); // Set the total number of universities
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -51,6 +57,10 @@ function App() {
 
     fetchGhanaianUniversities();
   }, []);
+
+  const onPageChange = (event: { first: number; }) => {
+    setFirst(event.first);
+  };
 
   return (
     <div className="app">
@@ -68,14 +78,20 @@ function App() {
             activeFilter={activeFilter}
           />
           <div className="uni-list">
-            {/* Render the list of Ghanaian universities */}
             <h2>Ghanaian Universities</h2>
             <ul>
-              {ghanaianUniversities.map((university) => (
+              {ghanaianUniversities.slice(first, first + rows).map((university) => (
                 <li key={university.name}>{university.name}</li>
               ))}
             </ul>
           </div>
+          <Paginator
+            first={first}
+            rows={1} // Display only one record per page
+            totalRecords={totalRecords}
+            onPageChange={onPageChange}
+            template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+          />
         </div>
       </div>
     </div>
