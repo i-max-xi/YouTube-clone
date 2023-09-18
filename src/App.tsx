53b7cb20beeee47ebf79d6886a72ce6750987dc6
Austin <appiaha66@gmail.com>
2023-09-18 08:32:42 +0000

    api call for universitries work

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
-import './styles/App.css';
-import Header from './components/Header';
-import SideBar from './components/SideBar';
-import FilterChips from './FilterChips';
+import { useEffect, useState } from "react";
+import "./styles/App.css";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import FilterChips from "./FilterChips";
 
 function App() {
-
   const filters = [
     "All",
     "Live",
@@ -19,9 +18,8 @@ function App() {
     "Recently uploaded",
     "New to you",
   ];
-  
-  const [activeFilter, setActiveFilter] = useState('All');
 
+  const [activeFilter, setActiveFilter] = useState("All");
 
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
@@ -29,21 +27,55 @@ function App() {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  // api call
+  type Universities = {
+    name : string;
+  }
+  const [ghanaianUniversities, setGhanaianUniversities] = useState<Universities[]>([]);
+
+  useEffect(() => {
+    const fetchGhanaianUniversities = async () => {
+      try {
+        const response = await fetch(
+          "http://universities.hipolabs.com/search?country=Ghana"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setGhanaianUniversities(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchGhanaianUniversities();
+  }, []);
+
   return (
     <div className="app">
-      <Header toggleSidebar={toggleSidebar} notificationCount={5}/>
+      <Header toggleSidebar={toggleSidebar} notificationCount={5} />
       <div className="app__content">
-      <SideBar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/>
+        <SideBar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
 
         <div className="app__main">
-        <FilterChips filters={filters} setActiveFilter={setActiveFilter} activeFilter={activeFilter}/>
-          {/* <div className="videoPlayer">
-            <video controls width="100%">
-              <source src="YOUR_VIDEO_URL" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </div> */}
-
+          <FilterChips
+            filters={filters}
+            setActiveFilter={setActiveFilter}
+            activeFilter={activeFilter}
+          />
+          <div className="uni-list">
+            {/* Render the list of Ghanaian universities */}
+            <h2>Ghanaian Universities</h2>
+            <ul>
+              {ghanaianUniversities.map((university) => (
+                <li key={university.name}>{university.name}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
